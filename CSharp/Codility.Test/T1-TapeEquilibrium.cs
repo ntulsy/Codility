9a408600f1259832baabccf0fcb77ee5768b94b7
ntulsy <ntulsy@live.com>
2015-10-05 00:44:33 +0800

    + C# TapeEquilibrium.

---
+++ CSharp/Codility.Test/T1-TapeEquilibrium.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TapeEquilibrium;
+
+namespace Codility.Test
+{
+    [TestClass]
+    public class TapeEquilibriumTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            int[] A = new int[] {1, 1};
+            int result = s.solution(A);
+            Assert.AreEqual(0, result);
+        }
+
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            int[] A = new int[] { 1, 1, 1 };
+            int result = s.solution(A);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            int[] A = new int[] { 1, 1, 1, 1 };
+            int result = s.solution(A);
+            Assert.AreEqual(0, result);
+        }
+
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution s = new Solution();
+            int[] A = new int[] { 3, 1, 2, 4, 3 };
+            int result = s.solution(A);
+            Assert.AreEqual(1, result);
+        }
+    }
+}
---
+++ CSharp/Codility/1-TapeEquilibrium.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TapeEquilibrium
+{
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            int[] SumLeft = new int[A.Length];
+            int[] SumRight = new int[A.Length];
+            SumLeft[0] = 0;
+            for (int i = 1; i < A.Length; ++i)
+            {
+                SumLeft[i] = SumLeft[i - 1] + A[i - 1];
+            }
+            SumRight[A.Length - 1] = A[A.Length - 1];
+            for (int i = A.Length - 2; i >= 0; --i)
+            {
+                SumRight[i] = SumRight[i + 1] + A[i];
+            }
+            int result = Int32.MaxValue;
+            for (int i = 1; i < A.Length; ++i)
+            {
+                int diff = Math.Abs(SumLeft[i] - SumRight[i]);
+                if (diff < result)
+                    result = diff;
+            }
+            return result;
+        }
+    }
+}
