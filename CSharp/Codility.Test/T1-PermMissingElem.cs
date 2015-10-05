fc89465430e66d27572248ec5f865cb6d7f1696e
ntulsy <ntulsy@live.com>
2015-10-06 00:47:27 +0800

    + PermMissingElem C#

---
+++ CSharp/Codility.Test/T1-PermMissingElem.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PermMissingElem;
+
+namespace Codility.Test
+{
+    [TestClass]
+    public class PermMissingElem
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            int[] A = new int[] { 2 };
+            int result = s.solution(A);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            int[] A = new int[] { 1, 3 };
+            int result = s.solution(A);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            int[] A = new int[] { 2, 3, 1, 5 };
+            int result = s.solution(A);
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution s = new Solution();
+            int[] A = Enumerable.Range(1, 100000).ToArray();
+            for (int i = 3; i < A.Length; ++i)
+            {
+                A[i] += 1;
+            }
+            int result = s.solution(A);
+            Assert.AreEqual(4, result);
+        }
+
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Solution s = new Solution();
+            int[] A = Enumerable.Range(1, 100000).ToArray();
+            for (int i = 0; i < A.Length; ++i)
+            {
+                A[i] += 1;
+            }
+            int result = s.solution(A);
+            Assert.AreEqual(1, result);
+        }
+    }
+}
---
+++ CSharp/Codility/1-PermMissingElem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PermMissingElem
+{
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            Int64 actualSum = A.Sum(a => Convert.ToInt64(a));
+            Int64 targetSum = Enumerable.Range(1, A.Length + 1).Sum(a => Convert.ToInt64(a));
+            return Convert.ToInt32(targetSum - actualSum);
+        }
+    }
+}
