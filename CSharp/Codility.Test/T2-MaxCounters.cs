d020fa37a91e1a000c09892b42bee939e5999bed
ntulsy <ntulsy@live.com>
2015-10-08 00:27:53 +0800

    + C# MaxCounters, 66%

---
+++ CSharp/Codility.Test/T2-MaxCounters.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MaxCounters;
+
+namespace Codility.Test
+{
+    [TestClass]
+    public class MaxCountersTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            int[] A = new int[] { 1 };
+            int[] result = s.solution(1, A);
+            int[] expectedResult = new int[] { 1 };
+            CollectionAssert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            int[] A = new int[] { 2 };
+            int[] result = s.solution(1, A);
+            int[] expectedResult = new int[] { 0 };
+            CollectionAssert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            int[] A = new int[] { 1, 2, 3, 4, 5, 6 };
+            int[] result = s.solution(6, A);
+            int[] expectedResult = new int[] { 1, 1, 1, 1, 1, 1 };
+            CollectionAssert.AreEqual(expectedResult, result);
+        }
+
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution s = new Solution();
+            int[] A = new int[] { 1, 1, 1, 1, 1, 2 };
+            int[] result = s.solution(4, A);
+            int[] expectedResult = new int[] { 5, 1, 0, 0 };
+            CollectionAssert.AreEqual(expectedResult, result);
+        }
+    }
+}
---
+++ CSharp/Codility/2-MaxCounters.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxCounters
+{
+    public class Solution
+    {
+        public int[] solution(int N, int[] A)
+        {
+            int[] counters = new int[N];
+            int currentMaxCounter = 0;
+            foreach (var a in A)
+            {
+                if (a == N + 1)
+                {
+                    currentMaxCounter += counters.Max();
+                    for (int i = 0; i < counters.Length; ++i)
+                    {
+                        counters[i] = 0;
+                    }
+                }
+                else
+                {
+                    ++counters[a - 1];
+                }
+            }
+            for (int i = 0; i < counters.Length; ++i)
+            {
+                counters[i] += currentMaxCounter;
+            }
+            return counters;
+        }
+    }
+}
