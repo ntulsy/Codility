c77cd707f304e14d33af9ba1d9210faa0fbfff15
ntulsy <ntulsy@live.com>
2015-10-13 00:19:14 +0800

    + C# Lesson 3 Done.

---
+++ CSharp/Codility.Test/T3-GenomicRangeQuery.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenomicRangeQuery;
+
+namespace Codility.Test
+{
+    [TestClass]
+    public class GenomicRangeQueryTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            String S = "CAGCCTA";
+            int[] P = new int[] { 2, 5, 0 };
+            int[] Q = new int[] { 4, 5, 6 };
+            int[] result = s.solution(S, P, Q);
+            int[] expected = { 2, 4, 1 };
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            String S = "AAAAAAA";
+            int[] P = new int[] { 1, 5, 0 };
+            int[] Q = new int[] { 4, 5, 6 };
+            int[] result = s.solution(S, P, Q);
+            int[] expected = { 1, 1, 1 };
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            String S = "TTTATTT";
+            int[] P = new int[] { 2, 5, 0 };
+            int[] Q = new int[] { 4, 5, 6 };
+            int[] result = s.solution(S, P, Q);
+            int[] expected = { 1, 4, 1 };
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution s = new Solution();
+            String S = "CAGCCTA";
+            int[] P = new int[] { 0, 5, 6 };
+            int[] Q = new int[] { 0, 5, 6 };
+            int[] result = s.solution(S, P, Q);
+            int[] expected = { 2, 4, 1 };
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Solution s = new Solution();
+            String S = "TC";
+            int[] P = new int[] { 0, 0, 1 };
+            int[] Q = new int[] { 0, 1, 1 };
+            int[] result = s.solution(S, P, Q);
+            int[] expected = { 4, 2, 2 };
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ CSharp/Codility/3-GenomicRangeQuery.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenomicRangeQuery
+{
+    public class Solution
+    {
+        private int getImpactFactor(char c)
+        {
+            switch(c)
+            {
+                case 'A':
+                    return 1;
+                case 'C':
+                    return 2;
+                case 'G':
+                    return 3;
+                case 'T':
+                    return 4;
+            }
+            throw new Exception("Invalid Genomic Sequence");
+        }
+        
+        private int[] getCountOfX(int[] sequence, int x)
+        {
+            int[] count = new int[sequence.Length];
+            for (int i = 1; i < sequence.Length; ++i)
+            {
+                count[i] = count[i - 1] + (sequence[i - 1] == x ? 1 : 0);
+            }
+            return count;
+        }
+
+        public int[] solution(string S, int[] P, int[] Q)
+        {
+            int[] sequence = new int[S.Length];
+            for (int i = 0; i < sequence.Length; ++i)
+            {
+                sequence[i] = getImpactFactor(S[i]);
+            }
+
+            int [][] classCount = new int[4][];
+            for (int i = 0; i < 4; ++i)
+            {
+                classCount[i] = getCountOfX(sequence, i + 1);
+            }
+
+            int[] result = new int[P.Length];
+            for (int i = 0; i < P.Length; ++i)
+            {
+                int left = P[i];
+                int right = Q[i];
+                if (left == right)
+                {
+                    result[i] = sequence[left];
+                    continue;
+                }
+                for (int j = 0; j < 4; ++j)
+                {
+                    if (classCount[j][left] != classCount[j][right] || sequence[right] == j + 1)
+                    {
+                        result[i] = j + 1;
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
