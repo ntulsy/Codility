af643e1229fe11bbf3b70c737a41004ac5b74233
ntulsy <ntulsy@live.com>
2015-10-14 23:56:11 +0800

    + C# L4Q3

---
+++ CSharp/Codility.Test/T4-Triangle.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Triangle;
+
+namespace Codility.Test
+{
+    [TestClass]
+    public class TriangleTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            int[] A = new int[] { 0, 0, 2 };
+            int result = s.solution(A);
+            Assert.AreEqual(0, result);
+        }
+
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            int[] A = new int[] { 1, 1, 1 };
+            int result = s.solution(A);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            int[] A = new int[] { 10, 2, 5, 1, 8, 20};
+            int result = s.solution(A);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution s = new Solution();
+            int[] A = new int[] { 10, 50, 1, 5 };
+            int result = s.solution(A);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Solution s = new Solution();
+            int[] A = new int[] { 2 };
+            int result = s.solution(A);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMetho6()
+        {
+            Solution s = new Solution();
+            int[] A = new int[] { int.MaxValue, int.MaxValue, int.MaxValue };
+            int result = s.solution(A);
+            Assert.AreEqual(1, result);
+        }
+    }
+}
---
+++ CSharp/Codility/4-Triangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            if (A.Length < 3)
+            { 
+                return 0;
+            }
+            Array.Sort(A);
+            for (int i = 2; i < A.Length; ++i)
+            {
+                if (canFormTriangle(A[i - 2], A[i - 1], A[i]))
+                {
+                    return 1;
+                }
+            }
+            return 0;
+        }
+        public bool canFormTriangle(long i, long j, long k)
+        {
+            return (i + j > k) && (j + k > i) && (i + k > j);
+        }
+    }
+}
