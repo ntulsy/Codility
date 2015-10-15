898aeda562278be7fb3c9a3eb09d7282f5cb10db
ntulsy <ntulsy@live.com>
2015-10-15 23:43:22 +0800

    + C# L5 Q1

---
+++ CSharp/Codility.Test/T5-Nesting.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nesting;
+
+namespace Codility.Test
+{
+    [TestClass]
+    public class NestingTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            String S = "";
+            int result = s.solution(S);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            String S = "(";
+            int result = s.solution(S);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            String S = ")";
+            int result = s.solution(S);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution s = new Solution();
+            String S = "()";
+            int result = s.solution(S);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Solution s = new Solution();
+            String S = "(()(())())";
+            int result = s.solution(S);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Solution s = new Solution();
+            String S = "())";
+            int result = s.solution(S);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Solution s = new Solution();
+            String S = "(()(())())(()(())())";
+            int result = s.solution(S);
+            Assert.AreEqual(1, result);
+        }
+    }
+}
---
+++ CSharp/Codility/5-Nesting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nesting
+{
+    public class Solution
+    {
+        public int solution(string S)
+        {
+            int counter = 0;
+            foreach (var s in S)
+            {
+                if (s == '(')
+                    ++counter;
+                else if (s == ')')
+                    --counter;
+                if (counter < 0)
+                    return 0;
+            }
+            return counter == 0 ? 1 : 0;
+        }
+    }
+}
