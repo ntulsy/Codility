a9277e539fc6c8c2649d788a14da25040a39daa7
ntulsy <ntulsy@live.com>
2015-10-16 23:41:58 +0800

    + C# L5 Q3

---
+++ CSharp/Codility.Test/T5-Brackets.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Brackets;
+
+namespace Codility.Test
+{
+    [TestClass]
+    public class BracketsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            String S = "";
+            int result = s.solution(S);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            String S = "(";
+            int result = s.solution(S);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            String S = ")";
+            int result = s.solution(S);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution s = new Solution();
+            String S = "(}";
+            int result = s.solution(S);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Solution s = new Solution();
+            String S = "{{}}{}[][]()()({[]})";
+            int result = s.solution(S);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Solution s = new Solution();
+            String S = "((({{{)))}}}";
+            int result = s.solution(S);
+            Assert.AreEqual(0, result);
+        }
+    }
+}
