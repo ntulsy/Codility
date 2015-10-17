e5d6a977c5543a5255038fabd7b1e2a842e84f28
ntulsy <ntulsy@live.com>
2015-10-18 00:02:07 +0800

    + C# L6 Q1

---
+++ CSharp/Codility.Test/T6-EquiLeader.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EquiLeader;
+
+namespace Codility.Test
+{
+    [TestClass]
+    public class EquiLeaderTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            int[] H = new int[] { 8, 8, 5, 7, 8, 8, 7, 4, 8 };
+            int result = s.solution(H);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            int[] H = new int[] { 1 };
+            int result = s.solution(H);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            int[] H = new int[] { 1, 1, 1, 1, 1, 1 };
+            int result = s.solution(H);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution s = new Solution();
+            int[] H = new int[] { 1, 1, 1, 1, 1, 5, 6, 4, 2 };
+            int result = s.solution(H);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Solution s = new Solution();
+            int[] H = new int[] { 5, 4, 3, 2, 1, 5, 5, 5, 5 };
+            int result = s.solution(H);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Solution s = new Solution();
+            int[] H = new int[] { 3, 4, 3, 2, 3, 1, 3, 0 };
+            int result = s.solution(H);
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Solution s = new Solution();
+            int[] H = new int[] { -1, 4, -1, 2, -1, 1, -1, 3 };
+            int result = s.solution(H);
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Solution s = new Solution();
+            int[] H = new int[] { -1, 4, -1, 2, -1, 1, -1, -1 };
+            int result = s.solution(H);
+            Assert.AreEqual(0, result);
+        }
+    }
+}
---
+++ CSharp/Codility/6-Dominator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominator
+{
+    public class Solution
+    {
+        public int solution(int[] H)
+        {
+            int stackValue = 0, stackCount = 0;
+            foreach(var h in H)
+            {
+                if (stackCount == 0)
+                {
+                    ++stackCount;
+                    stackValue = h;
+                }
+                else if (stackValue != h)
+                    --stackCount;
+                else if (stackValue == h)
+                    ++stackCount;
+            }
+            if (stackCount > 0)
+            {
+                int count = 0;
+                foreach(var h in H)
+                {
+                    if (h == stackValue)
+                        ++count;
+                }
+                if (count > H.Length / 2)
+                    return Array.IndexOf(H, stackValue);
+                else
+                    return -1;
+            }
+            return -1;
+        }
+    }
+}
