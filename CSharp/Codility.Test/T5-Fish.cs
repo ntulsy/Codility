f5bf50f387a766a640f768f1b0cccd5f9dc8795c
ntulsy <ntulsy@live.com>
2015-10-17 17:25:41 +0800

    + C# Lesson 4 Done.

---
+++ CSharp/Codility.Test/T5-Fish.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fish;
+
+namespace Codility.Test
+{
+    [TestClass]
+    public class FishTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            int[] A = { 10 };
+            int[] B = { 0 };
+            int result = s.solution(A, B);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            int[] A = { 10 };
+            int[] B = { 1 };
+            int result = s.solution(A, B);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            int[] A = { 10, 11, 2, 3, 4, 5 };
+            int[] B = { 1, 1, 1, 1, 1, 1 };
+            int result = s.solution(A, B);
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution s = new Solution();
+            int[] A = { 10, 11, 2, 3, 4, 5 };
+            int[] B = { 0, 0, 0, 0, 0, 0 };
+            int result = s.solution(A, B);
+            Assert.AreEqual(6, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Solution s = new Solution();
+            int[] A = { 10, 11, 2, 3, 4, 5 };
+            int[] B = { 0, 0, 1, 1, 0, 0 };
+            int result = s.solution(A, B);
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Solution s = new Solution();
+            int[] A = { 10, 11, 2, 3, 4, 5 };
+            int[] B = { 1, 0, 1, 1, 0, 0 };
+            int result = s.solution(A, B);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Solution s = new Solution();
+            int[] A = { 4, 3, 2, 1, 5 };
+            int[] B = { 0, 1, 0, 0, 0 };
+            int result = s.solution(A, B);
+            Assert.AreEqual(2, result);
+        }
+    }
+}
---
+++ CSharp/Codility/5-Fish.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fish
+{
+    public class Solution
+    {
+        public int solution(int[] A, int[] B)
+        {
+            Stack<KeyValuePair<int, int>> remainingFish = new Stack<KeyValuePair<int, int>>();
+            for (int i = 0; i < A.Length; ++i)
+            {
+                bool isEaten = false;
+                if (B[i] == 0)
+                {
+                    while (remainingFish.Count != 0)
+                    {
+                        if (remainingFish.Peek().Value == 1)
+                        {
+                            if (remainingFish.Peek().Key < A[i])
+                                remainingFish.Pop();
+                            else
+                            {
+                                isEaten = true;
+                                break;
+                            }
+                        }
+                        else
+                            break;
+                    }
+                }
+                if (!isEaten)
+                    remainingFish.Push(new KeyValuePair<int, int>(A[i], B[i]));
+            }
+            return remainingFish.Count;
+        }
+    }
+}
