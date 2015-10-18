edf3b5e2f4bc814b3803b4b6ff6bef64a0c7f44b
ntulsy <ntulsy@live.com>
2015-10-18 23:27:47 +0800

    +C# L6 Done.

--- CSharp/Codility.Test/T6-EquiLeader.cs
+++ CSharp/Codility.Test/T6-EquiLeader.cs
@@ -15,9 +15,9 @@ namespace Codility.Test
         public void TestMethod1()
         {
             Solution s = new Solution();
-            int[] H = new int[] { 8, 8, 5, 7, 8, 8, 7, 4, 8 };
+            int[] H = new int[] { 4, 3, 4, 4, 4, 2 };
             int result = s.solution(H);
-            Assert.AreEqual(0, result);
+            Assert.AreEqual(2, result);
         }
 
         [TestMethod]
@@ -35,52 +35,16 @@ namespace Codility.Test
             Solution s = new Solution();
             int[] H = new int[] { 1, 1, 1, 1, 1, 1 };
             int result = s.solution(H);
-            Assert.AreEqual(0, result);
+            Assert.AreEqual(5, result);
         }
 
         [TestMethod]
         public void TestMethod4()
         {
             Solution s = new Solution();
-            int[] H = new int[] { 1, 1, 1, 1, 1, 5, 6, 4, 2 };
+            int[] H = new int[] { 1, 1 };
             int result = s.solution(H);
-            Assert.AreEqual(0, result);
-        }
-
-        [TestMethod]
-        public void TestMethod5()
-        {
-            Solution s = new Solution();
-            int[] H = new int[] { 5, 4, 3, 2, 1, 5, 5, 5, 5 };
-            int result = s.solution(H);
-            Assert.AreEqual(0, result);
-        }
-
-        [TestMethod]
-        public void TestMethod6()
-        {
-            Solution s = new Solution();
-            int[] H = new int[] { 3, 4, 3, 2, 3, 1, 3, 0 };
-            int result = s.solution(H);
-            Assert.AreEqual(-1, result);
-        }
-
-        [TestMethod]
-        public void TestMethod7()
-        {
-            Solution s = new Solution();
-            int[] H = new int[] { -1, 4, -1, 2, -1, 1, -1, 3 };
-            int result = s.solution(H);
-            Assert.AreEqual(-1, result);
-        }
-
-        [TestMethod]
-        public void TestMethod8()
-        {
-            Solution s = new Solution();
-            int[] H = new int[] { -1, 4, -1, 2, -1, 1, -1, -1 };
-            int result = s.solution(H);
-            Assert.AreEqual(0, result);
+            Assert.AreEqual(1, result);
         }
     }
 }
