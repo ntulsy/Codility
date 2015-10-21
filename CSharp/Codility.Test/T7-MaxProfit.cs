07f033d90dedebb7db6191e9dab64b8ce5c7c2fa
ntulsy <ntulsy@live.com>
2015-10-21 23:07:17 +0800

    + C# L7Q2

---
+++ CSharp/Codility.Test/T7-MaxProfit.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MaxProfit;
+
+namespace Codility.Test
+{
+    [TestClass]
+    public class MaxProfitTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            int[] H = new int[] { };
+            int result = s.solution(H);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            int[] H = new int[] { 1 };
+            int result = s.solution(H);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            int[] H = new int[] { 1, 5 };
+            int result = s.solution(H);
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution s = new Solution();
+            int[] H = new int[] { 5, 1 };
+            int result = s.solution(H);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Solution s = new Solution();
+            int[] H = new int[] { 23171, 21011, 21123, 21366, 21013, 21367 };
+            int result = s.solution(H);
+            Assert.AreEqual(356, result);
+        }
+    }
+}
---
+++ CSharp/Codility/7-MaxProfit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxProfit
+{
+    // solved with external assistance
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            int maxProfitCurrent = 0;
+            int maxProfitTotal = 0;
+            for (int i = 1; i < A.Length; ++i)
+            {
+                maxProfitCurrent = Math.Max(0, maxProfitCurrent + A[i] - A[i - 1]);
+                maxProfitTotal = Math.Max(maxProfitTotal, maxProfitCurrent);
+            }
+            return maxProfitTotal;
+        }
+    }
+}
