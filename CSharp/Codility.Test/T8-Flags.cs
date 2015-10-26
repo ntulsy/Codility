9b279b4c4e6c2eddd87a786ce88a7b7ae6f23431
ntulsy <ntulsy@live.com>
2015-10-26 23:37:08 +0800

    C# L8 Q4 IP

---
+++ CSharp/Codility.Test/T8-Flags.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Flags;
+
+namespace Codility.Test
+{
+    [TestClass]
+    public class FlagsTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            int[] A = new int[] { 1, 1, 1, 1, 1, 1, 1 };
+            int result = s.solution(A);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            int[] A = new int[] { 1, 1, 1, 2, 1, 1, 1 };
+            int result = s.solution(A);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            int[] A = new int[] { 1, 1, 1, 2, 1, 2, 1 };
+            int result = s.solution(A);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution s = new Solution();
+            int[] A = new int[] { 1, 2, 1, 1, 1, 1, 2, 1 };
+            int result = s.solution(A);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Solution s = new Solution();
+            int[] A = new int[] { 1, 2, 3, 4, 3, 4, 1, 2, 3, 4, 6, 2 };
+            int result = s.solution(A);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ CSharp/Codility/8-Flags.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flags
+{
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            bool[] peaks = new bool[A.Length];
+
+            for (int i = 1; i < A.Length - 1; ++i)
+            {
+                if (A[i - 1] < A[i] && A[i] > A[i + 1])
+                    peaks[i] = true;
+            }
+
+            int maxFlagCount = Math.Max(peaks.Length, Convert.ToInt32(Math.Floor(Math.Sqrt(A.Length))));
+            return 0;
+        }
+    }
+}
