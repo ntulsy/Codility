25f12f37b99a410eac10656345e4846a02806320
ntulsy <ntulsy@live.com>
2015-11-01 23:20:43 +0800

    + C# L9 Q1.

---
+++ CSharp/Codility.Test/T9-Semiprimes.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Semiprimes;
+
+namespace Codility.Test
+{
+    [TestClass]
+    public class SemiprimesTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            int[] P = new int[] { 1, 1, 1, 1, 1, 1, 1 };
+            int[] Q = new int[] { 1, 1, 1, 1, 1, 1, 1 };
+            int[] result = s.solution(10, P, Q);
+            int[] expected = { 0, 0, 0, 0, 0, 0, 0 };
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            int[] P = new int[] { 1, 4, 16 };
+            int[] Q = new int[] { 26, 10, 20 };
+            int[] result = s.solution(26, P, Q);
+            int[] expected = { 10, 4, 0 };
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            int[] P = new int[] { 1, 1, 1, 1, 1, 1, 1 };
+            int[] Q = new int[] { 1, 1, 1, 1, 1, 1, 1 };
+            int[] result = s.solution(50000, P, Q);
+            int[] expected = { 0, 0, 0, 0, 0, 0, 0 };
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ CSharp/Codility/9-Semiprimes.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semiprimes
+{
+    public class Solution
+    {
+        public int[] solution(int N, int[] P, int[] Q)
+        {
+            int[] primes = getPrimes(N);
+            bool[] isSubprime = Enumerable.Repeat<bool>(false, N + 1).ToArray();
+            for (int i = 0; i < primes.Length; ++i)
+            {
+                for (int j = i;
+                    primes[j] * primes[i] <= N && primes[j] * primes[i] > 0 && j < primes.Length;
+                    ++j)
+                {
+                    isSubprime[primes[j] * primes[i]] = true;
+                }
+            }
+            int[] subprimeCount = new int[N + 1];
+            for (int i = 1; i < N + 1; ++i)
+            {
+                subprimeCount[i] = subprimeCount[i - 1] + (isSubprime[i] ? 1 : 0);
+            }
+            int[] returnValues = new int[P.Length];
+            for (int i = 0; i < P.Length; ++i)
+            {
+                returnValues[i] = subprimeCount[Q[i]] - subprimeCount[P[i]] + (isSubprime[P[i]] ? 1 : 0);
+            }
+            return returnValues;
+        }
+
+        private int[] getPrimes(int n)
+        {
+            bool[] isPrime = Enumerable.Repeat<bool>(true, n + 1).ToArray();
+            isPrime[0] = isPrime[1] = false;
+            for (int i = 2; i * i <= n; ++i)
+            {
+                for (int j = i * i; j <= n; j += i)
+                {
+                    isPrime[j] = false;
+                }
+            }
+            return isPrime
+                .Select((value, index) => new { Value = value, Index = index })
+                .Where(x => x.Value)
+                .Select(x => x.Index)
+                .ToArray();
+        }
+    }
+}
