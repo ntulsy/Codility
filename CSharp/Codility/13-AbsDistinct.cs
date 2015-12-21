2801dcd736ce9bcdcbf2102d9a723f8c4fcbb205
ntulsy <ntulsy@live.com>
2015-12-22 00:51:10 +0800

    C# L13 Q1

---
+++ CSharp/Codility/13-AbsDistinct.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbsDistinct
+{
+    public class Solution
+    {
+        public int solution(int[] A)
+        {
+            int hasMin = 0;
+            Dictionary<int, bool> dict = new Dictionary<int, bool>();
+            for (int i = 0; i < A.Length; ++i)
+            {
+                if (A[i] == Int32.MinValue)
+                    hasMin = 1;
+                else
+                    dict[Math.Abs(A[i])] = true;
+            }
+            return dict.Count + hasMin;
+        }
+    }
+}
