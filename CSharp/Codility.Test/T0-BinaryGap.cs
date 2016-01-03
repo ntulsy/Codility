9d297aece294ff1e48d42d2b18693573f9a3a461
ntulsy <ntulsy@live.com>
2016-01-03 14:48:26 +0800

    + Additional Lesson 1 and 2, marked as Lesson 0.

---
+++ CSharp/Codility.Test/T0-BinaryGap.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BinaryGap;
+
+namespace Codility.Test
+{
+    [TestClass]
+    public class BinaryGapTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            int result = s.solution(1041);
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            int result = s.solution(9);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            int result = s.solution(529);
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution s = new Solution();
+            int result = s.solution(15);
+            Assert.AreEqual(0, result);
+        }
+    }
+}
---
+++ CSharp/Codility/0-BinaryGap.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryGap
+{
+    public class Solution
+    {
+        public int solution(int N)
+        {
+            string binaryRepresentation = Convert.ToString(N, 2);
+            int max = 0;
+            int current = 0;
+            foreach (char c in binaryRepresentation){
+                if (c == '1')
+                {
+                    if (current > max)
+                        max = current;
+                    current = 0;
+                }
+                else
+                    ++current;
+            }
+            return max;
+        }
+    }
+}
