c94212a6780a9273de6d900b1a427795020c1041
ntulsy <ntulsy@live.com>
2016-01-16 23:04:29 +0800

    + C# Array.

---
+++ CSharp/Codility.Test/T2-CyclicRotation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CyclicRotation;
+
+namespace Codility.Test
+{
+    [TestClass]
+    public class CyclicRotationTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            int[] A = { 3, 8, 9, 7, 6 };
+            int[] expected = { 9, 7, 6, 3, 8 };
+            int[] result = s.solution(A, 3);
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            int[] A = { };
+            int[] expected = { };
+            int[] result = s.solution(A, 3);
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            int[] A = { 3 };
+            int[] expected = { 3 };
+            int[] result = s.solution(A, 3);
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution s = new Solution();
+            int[] A = { 3, 8, 9, 7, 6 };
+            int[] expected = { 9, 7, 6, 3, 8 };
+            int[] result = s.solution(A, 13);
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
