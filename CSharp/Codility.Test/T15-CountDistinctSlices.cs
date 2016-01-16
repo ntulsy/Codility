4d4c9e404836423d564c273629f9dfc69a1f2abb
ntulsy <ntulsy@live.com>
2016-01-17 00:06:58 +0800

    + C# L15 Q3.

---
+++ CSharp/Codility.Test/T15-CountDistinctSlices.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CountDistinctSlices;
+
+namespace Codility.Test
+{
+    [TestClass]
+    public class CountDistinctSlicesTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            int[] A = { 3, 4, 5, 5, 2 };
+            int result = s.solution(6, A);
+            Assert.AreEqual(9, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            int[] A = { 1 };
+            int result = s.solution(10, A);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            int[] A = { 1, 1, 1, 1 };
+            int result = s.solution(4, A);
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution s = new Solution();
+            int[] A = { 1, 2, 3, 1, 5 };
+            int result = s.solution(10, A);
+            Assert.AreEqual(13, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Solution s = new Solution();
+            int[] A = Enumerable.Range(1, 100000).ToArray();
+            int result = s.solution(100000, A);
+            Assert.AreEqual((int) 1e9, result);
+        }
+    }
+}
---
+++ CSharp/Codility/15-CountDistinctSlices.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountDistinctSlices
+{
+    public class Solution
+    {
+        public int solution(int M, int[] A)
+        {
+            int sum = 0;
+            bool[] hasAppeared = new bool[M + 1];
+            int start = 0, end = 0;
+            while (start < A.Length)
+            {
+                if (end < A.Length)
+                {
+                    if (hasAppeared[A[end]])
+                    {
+                        sum += (end - start);
+                        hasAppeared[A[start++]] = false;
+
+                        if (sum > 1e9)
+                            return (int)1e9;
+                    }
+                    else
+                    {
+                        hasAppeared[A[end]] = true;
+                        ++end;
+                    }
+                }
+                else
+                {
+                    while (start < A.Length)
+                    {
+                        sum += (end - start);
+                        ++start;
+
+                        if (sum > 1e9)
+                            return (int)1e9;
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
