a56cffb1ff7f6fec8574fc2909ca8324019df88c
ntulsy <ntulsy@live.com>
2016-01-31 00:32:58 +0800

    C# L99 Q1-2

---
+++ CSharp/Codility.Test/T99-TreeHeight.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TreeHeight;
+
+namespace Codility.Test
+{
+    [TestClass]
+    public class TreeHeightTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            Tree T = null;
+            int result = s.solution(T);
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            Tree T = new Tree { x = 5 };
+            int result = s.solution(T);
+            Assert.AreEqual(0, result);
+        }
+
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            Tree T = new Tree { x = 5 };
+            Tree L1 = new Tree { x = 3 };
+            Tree R1 = new Tree { x = 10 };
+            Tree L2 = new Tree { x = 20 };
+            Tree L3 = new Tree { x = 21 };
+            Tree R2 = new Tree { x = 1 };
+            T.l = L1;
+            T.r = R1;
+            L1.l = L2;
+            L1.r = L3;
+            R1.l = R2;
+            int result = s.solution(T);
+            Assert.AreEqual(2, result);
+        }
+    }
+}
---
+++ CSharp/Codility/99-TreeHeight.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeHeight
+{
+    public class Solution
+    {
+        public int solution(Tree T)
+        {
+            if (T == null)
+                return -1;
+            return Math.Max(solution(T.l), solution(T.r)) + 1;
+        }
+    }
+
+    public class Tree
+    {
+        public int x;
+        public Tree l;
+        public Tree r;
+    }
+}
