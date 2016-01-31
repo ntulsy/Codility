5af81577b08180995689f0e29e6e85144450c373
ntulsy <ntulsy@live.com>
2016-01-31 21:58:08 +0800

    C# L13 Done

---
+++ CSharp/Codility.Test/T13-FibFrog.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FibFrog;
+
+namespace Codility.Test
+{
+    [TestClass]
+    public class FibFrogTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            int[] A = { 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0 };
+            int result = s.solution(A);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            int[] A = { };
+            int result = s.solution(A);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            int[] A = { 1 };
+            int result = s.solution(A);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution s = new Solution();
+            int[] A = { 0 };
+            int result = s.solution(A);
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Solution s = new Solution();
+            int[] A = { 0, 0, 1, 0, 0, 0, 0, 0 };
+            int result = s.solution(A);
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Solution s = new Solution();
+            int[] A = { 1, 0, 1, 0, 1, 0, 1, 0 };
+            int result = s.solution(A);
+            Assert.AreEqual(2, result);
+        }
+    }
+}
