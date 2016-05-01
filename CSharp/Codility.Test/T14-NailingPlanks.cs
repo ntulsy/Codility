2f1b64862ba3ad3ebbd9c111f017e4b1234080d2
ntulsy <ntulsy@live.com>
2016-05-01 22:20:56 +0800

    14-NailingPlanks 87%. One TLE.

---
+++ CSharp/Codility.Test/T14-NailingPlanks.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NailingPlanks;
+
+namespace Codility.Test
+{
+    [TestClass]
+    public class NailingPlanksTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            int[] A = { 1, 4, 5, 8 };
+            int[] B = { 4, 5, 9, 10 };
+            int[] C = { 4, 6, 7, 10, 2 };
+            int result = s.solution(A, B, C);
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            int[] A = { 1 };
+            int[] B = { 4 };
+            int[] C = { 4, 6, 7, 10, 2 };
+            int result = s.solution(A, B, C);
+            Assert.AreEqual(1, result);
+        }
+
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            int[] A = { 11 };
+            int[] B = { 14 };
+            int[] C = { 4, 6, 7, 10, 2 };
+            int result = s.solution(A, B, C);
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution s = new Solution();
+            int[] A = { 1, 4, 5, 8 ,30 };
+            int[] B = { 4, 5, 9, 10, 32 };
+            int[] C = { 4, 6, 7, 10, 2 };
+            int result = s.solution(A, B, C);
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Solution s = new Solution();
+            int[] A = { 1 };
+            int[] B = { 1 };
+            int[] C = { 1 };
+            int result = s.solution(A, B, C);
+            Assert.AreEqual(1, result);
+        }
+    }
+}
---
+++ CSharp/Codility/14-NailingPlanks.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NailingPlanks
+{
+    public class Solution
+    {
+        public int solution(int[] A, int[] B, int[] C)
+        {
+            int result = -1;
+            var nails = C
+                .Select((x, i) => new KeyValuePair<int, int>(x, i))
+                .OrderBy(x => x.Key)
+                .ToList();
+            for (int i = 0; i < A.Length; i++)
+            {
+                var plankResult = findNailForPlank(A[i], B[i], nails, result);
+                if (plankResult == -1)
+                    return -1;
+                else
+                    result = Math.Max(result, plankResult);
+            }
+            return result + 1;
+        }
+
+        private int findNailForPlank(int plankStart, int plankEnd, List<KeyValuePair<int, int>> nails, int previousResult)
+        {
+            int lowerBound = 0, upperBound = nails.Count - 1, mid = 0;
+            bool found = false;
+            while (!found && upperBound >= lowerBound)
+            {
+                mid = (upperBound + lowerBound) / 2;
+                int nailPosition = nails[mid].Key;
+                if (nailPosition < plankStart)
+                    lowerBound = mid + 1;
+                else if (nailPosition > plankEnd)
+                    upperBound = mid - 1;
+                else
+                    found = true;
+            }
+            if (!found)
+                return -1;
+            int firstNail = nails.Count;
+            for (int i = mid; i < nails.Count && nailOnPlank(nails[i], plankStart, plankEnd); i++)
+            {
+                firstNail = Math.Min(nails[i].Value, firstNail);
+                if (firstNail < previousResult)
+                    return previousResult;
+            }
+            for (int i = mid; i >= 0 && nailOnPlank(nails[i], plankStart, plankEnd); i--)
+            {
+                firstNail = Math.Min(nails[i].Value, firstNail);
+                if (firstNail < previousResult)
+                    return previousResult;
+            }
+            return firstNail;
+        }
+
+        private bool nailOnPlank(KeyValuePair<int, int> nail, int plankStart, int plankEnd)
+        {
+            return nail.Key >= plankStart && nail.Key <= plankEnd;
+        }
+    }
+}
