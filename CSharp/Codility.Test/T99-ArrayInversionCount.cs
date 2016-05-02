5689302e612eaed1533eed678a6a5abd67d73fa8
ntulsy <ntulsy@live.com>
2016-05-02 16:00:42 +0800

    L99 Q3

---
+++ CSharp/Codility.Test/T99-ArrayInversionCount.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArrayInversionCount;
+
+namespace Codility.Test
+{
+    [TestClass]
+    public class ArrayInversionCountTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            int[] A = { };
+            int result = s.solution(A);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            int[] A = { 1 };
+            int result = s.solution(A);
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            int[] A = { 1, 2, 3, 4, 5, 6, 7 };
+            int result = s.solution(A);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution s = new Solution();
+            int[] A = { -1, 6, 3, 4, 7, 4 };
+            int result = s.solution(A);
+            Assert.AreEqual(4, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Solution s = new Solution();
+            int[] A = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+            int result = s.solution(A);
+            Assert.AreEqual(9, result);
+        }
+    }
+}
---
+++ CSharp/Codility/99-ArrayInversionCount.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayInversionCount
+{
+    public class Solution
+    {
+        private static int MAX = (int)1E9;
+        public int solution(int[] A)
+        {
+            return (int) mergeSort(A, 0, A.Length - 1);
+        }
+
+        private long mergeSort(int[] A, int left, int right)
+        {
+            if (left >= right)
+                return 0;
+            int mid = (left + right) / 2;
+            long leftCount = mergeSort(A, left, mid);
+            if (leftCount == -1)
+                return -1;
+            long rightCount = mergeSort(A, mid + 1, right);
+            if (rightCount == -1)
+                return -1;
+
+            long currentCount = 0;
+            int i = left, j = mid + 1;
+            Queue<int> temp = new Queue<int>();
+            while (i <= mid && j <= right)
+            {
+                if (A[i] <= A[j])
+                    temp.Enqueue(A[i++]);
+                else
+                {
+                    currentCount += (mid - i + 1);
+                    if (currentCount > MAX)
+                        return -1;
+                    temp.Enqueue(A[j++]);
+                }
+            }
+
+            while (i <= mid)
+                temp.Enqueue(A[i++]);
+            while (j <= right)
+                temp.Enqueue(A[j++]);
+
+            for (int k = left; k <= right; ++k)
+                A[k] = temp.Dequeue();
+
+            long totalCount = leftCount + rightCount + currentCount;
+            return totalCount > MAX ? -1 : totalCount;
+        }
+    }
+}
