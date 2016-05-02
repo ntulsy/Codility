797c9e7a658a0607cf9ea4e6ebc4849df3b6d1d7
ntulsy <ntulsy@live.com>
2016-05-03 01:19:35 +0800

    L90 Q2

---
+++ CSharp/Codility.Test/T90-RectangleBuilderGreaterArea.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RectangleBuilderGreaterArea;
+
+namespace Codility.Test
+{
+    [TestClass]
+    public class RectangleBuilderGreaterAreaTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            int X = 5;
+            int[] A = { 1, 2, 5, 1, 1, 2, 3, 5, 1 };
+            int result = s.solution(A, X);
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            int X = 1000;
+            int[] A = { 1, 2, 5, 1, 1, 2, 3, 5, 1 };
+            int result = s.solution(A, X);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            int X = 1000;
+            int[] A = { };
+            int result = s.solution(A, X);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution s = new Solution();
+            int X = 5;
+            int[] A = { 1, 2, 5, 1, 1, 2, 3, 5, 1, 5, 5 };
+            int result = s.solution(A, X);
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Solution s = new Solution();
+            int X = 5;
+            int[] A = { 1, 2, 5, 1, 1, 2, 3, 5, 1, 5, 5, 1, 1, 1, 1, 1, 1, 5, 5, 5 };
+            int result = s.solution(A, X);
+            Assert.AreEqual(3, result);
+        }
+    }
+}
---
+++ CSharp/Codility/90-RectangleBuilderGreaterArea.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectangleBuilderGreaterArea
+{
+    public class Solution
+    {
+        private const int MAX = (int)1E9;
+        public int solution(int[] A, int X)
+        {
+            long count = 0;
+            var occurenceCount = A
+                .GroupBy(x => x)
+                .Where(x => x.Count() > 1)
+                .Select(x => new KeyValuePair<int, int>(x.Key, Math.Min(x.Count() / 2, 2)))
+                .OrderBy(x => x.Key)
+                .ToList();
+            for (int i = 0; i < occurenceCount.Count; i++)
+            {
+                var targetMultiplier = (int)Math.Ceiling(((double)X) / ((double)occurenceCount[i].Key));
+                // case: more than four pieces with the same length
+                var left = occurenceCount[i].Value > 1 ? i : i + 1;
+                // It is already the last piece. Array out of bound
+                if (left >= occurenceCount.Count)
+                    break;
+                var right = occurenceCount.Count - 1;
+                var index = searchForMultiplier(occurenceCount, left, right, targetMultiplier);
+                count += (occurenceCount.Count - index);
+                if (count > MAX)
+                    return -1;
+            }
+            return (int)count;
+        }
+
+        private int searchForMultiplier(List<KeyValuePair<int, int>> occurenceCount, int left, int right, int target)
+        {
+            while (left < right){
+                int mid = (left + right) / 2;
+                if (occurenceCount[mid].Key < target)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+            if (occurenceCount[left].Key < target)
+                ++left;
+            return left;
+        }
+    }
+}
