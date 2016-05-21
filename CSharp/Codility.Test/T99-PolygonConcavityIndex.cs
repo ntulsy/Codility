4aff8983691ad741eb9d7c6dabe22049817d16e6
ntulsy <ntulsy@live.com>
2016-05-21 17:02:31 +0800

    C# L99 done.

---
+++ CSharp/Codility.Test/T99-PolygonConcavityIndex.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PolygonConcavityIndex;
+
+namespace Codility.Test
+{
+    [TestClass]
+    public class PolygonConcavityIndexTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Solution s = new Solution();
+            List<Point2D> A = new List<Point2D>();
+            A.Add(new Point2D { x = -1, y = 3 });
+            A.Add(new Point2D { x = 3, y = 1 });
+            A.Add(new Point2D { x = 0, y = -1 });
+            A.Add(new Point2D { x = -2, y = 1 });
+            int result = s.solution(A.ToArray());
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Solution s = new Solution();
+            List<Point2D> A = new List<Point2D>();
+            A.Add(new Point2D { x = 1, y = 1 });
+            A.Add(new Point2D { x = 2, y = 2 });
+            A.Add(new Point2D { x = 3, y = 3 });
+            int result = s.solution(A.ToArray());
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Solution s = new Solution();
+            List<Point2D> A = new List<Point2D>();
+            A.Add(new Point2D { x = 0, y = 0 });
+            A.Add(new Point2D { x = 2, y = 0 });
+            A.Add(new Point2D { x = 3, y = 1 });
+            A.Add(new Point2D { x = 2, y = 2 });
+            A.Add(new Point2D { x = 0, y = 2 });
+            int result = s.solution(A.ToArray());
+            Assert.AreEqual(-1, result);
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Solution s = new Solution();
+            List<Point2D> A = new List<Point2D>();
+            A.Add(new Point2D { x = 0, y = 0 });
+            A.Add(new Point2D { x = 2, y = 0 });
+            A.Add(new Point2D { x = 3, y = 1 });
+            A.Add(new Point2D { x = 2, y = 2 });
+            A.Add(new Point2D { x = 0, y = 2 });
+            A.Add(new Point2D { x = 1, y = 1 });
+            int result = s.solution(A.ToArray());
+            Assert.AreEqual(5, result);
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+            Solution s = new Solution();
+            List<Point2D> A = new List<Point2D>();
+            A.Add(new Point2D { x = 0, y = 0 });
+            A.Add(new Point2D { x = 0, y = 3 });
+            A.Add(new Point2D { x = 3, y = 3 });
+            A.Add(new Point2D { x = 3, y = 0 });
+            int result = s.solution(A.ToArray());
+            Assert.AreEqual(-1, result);
+        }
+    }
+}
---
+++ CSharp/Codility/99-PolygonConcavityIndex.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolygonConcavityIndex
+{
+    public class Solution
+    {
+        private const int STRAIGHT = 0;
+        private const int LEFT = -1;
+        private const int RIGHT = 1;
+        public int solution(Point2D[] A)
+        {
+            int minY = A[0].y;
+            List<int> minYIndex = new List<int>();
+            for (int i = 0; i < A.Length; ++i)
+            {
+                if (A[i].y < minY)
+                {
+                    minY = A[i].y;
+                    minYIndex.Clear();
+                    minYIndex.Add(i);
+                }
+                else if (A[i].y == minY)
+                    minYIndex.Add(i);
+            }
+
+            int start = minYIndex[0];
+            for (int i = 1; i < minYIndex.Count; ++i)
+            {
+                if (start == minYIndex[i] - 1)
+                    start = minYIndex[i];
+            }
+
+            int direction = getDirection(A[(start - 1 + A.Length) % A.Length], A[start], A[(start + 1) % A.Length]);
+
+            for (int i = start; i < A.Length + start; i++)
+            {
+                if (direction * getDirection(A[(i - 1 + A.Length) % A.Length], A[i % A.Length], A[(i + 1) % A.Length]) < 0)
+                    return i % A.Length;
+            }
+            return -1;
+        }
+
+        private int getDirection(Point2D A, Point2D B, Point2D C)
+        {
+            long crossProduct = ((long)B.x - (long)A.x) * ((long)C.y - (long)A.y) - ((long)C.x - (long)A.x) * ((long)B.y - (long)A.y);
+            return crossProduct < 0 ? RIGHT : (crossProduct > 0 ? LEFT : STRAIGHT);
+        }
+    }
+
+    public class Point2D
+    {
+        public int x;
+        public int y;
+    }
+}
